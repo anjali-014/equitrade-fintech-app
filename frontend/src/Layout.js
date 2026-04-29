0b353c0a2692b34ea91edab4c52b0cbf7620fb5c
Anjali Saini <anjalisaini0192@gmail.com>
2026-04-29 20:14:37 +0530

    Improved UI

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./landing_page/Navbar";
  import Footer from "./landing_page/Footer";
 
+//layout
 const Layout = () => {
   return (
     <div className="layout">
